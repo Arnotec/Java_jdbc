6f96fc7ca3872cc723bb9b314345a1f04940ff6d
Arnaud <tsofackarnaud@gmail.com>
2020-08-11 14:39:26 +0400

    jdbc DriverManager - Transactions

--- src/main/java/com/arnotec/org/TestDeConnection.java
+++ src/main/java/com/arnotec/org/TestDeConnection.java
@@ -1,22 +1,38 @@
 package com.arnotec.org;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class TestDeConnection {
     public static void main(String... args){
         Connection conn = null;
         try {
-            //Seulement avant Java 7/JDBC 4
-            //Class.forName(DRIVER_CLASS_NAME);
-
             //MySQL driver MySQL Connector
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris","root","root");
-            //Oracle Driver officiel OJDBC Thin
-            //conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:tennis","COURSDB","COURSDB");
-            //Postgres Driver officiel
-            //conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/tennis","COURSDB","COURSDB");
+
+            conn.setAutoCommit(false);
+
+            PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO JOUEUR (NOM,PRENOM,SEXE) VALUES (?,?,?)");
+
+            String nom = "Capriati";
+            String prenom = "Jennifer";
+            String sexe = "F";
+            preparedStatement.setString(1, nom);
+            preparedStatement.setString(2, prenom);
+            preparedStatement.setString(3, sexe);
+
+            preparedStatement.executeUpdate();
+
+            nom = "Johannson";
+            prenom = "Thomas";
+            sexe = "M";
+            preparedStatement.setString(1, nom);
+            preparedStatement.setString(2, prenom);
+            preparedStatement.setString(3, sexe);
+
+            preparedStatement.executeUpdate();
+
+            conn.commit();
+
             System.out.println("success");
         } catch (SQLException e) {
             e.printStackTrace();
@@ -28,6 +44,11 @@ public class TestDeConnection {
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
+                try {
+                    if(conn!=null) conn.rollback();
+                } catch (SQLException e1) {
+                    e1.printStackTrace();
+                }
             }
         }
     }
