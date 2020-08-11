c0292cf822730a8a46a2348017fd0d4e597a0b51
Arnaud <tsofackarnaud@gmail.com>
2020-08-11 14:55:15 +0400

    jdbc DataSource - basic

--- src/main/java/com/arnotec/org/TestDeConnection.java
+++ src/main/java/com/arnotec/org/TestDeConnection.java
@@ -1,39 +1,51 @@
 package com.arnotec.org;
 
+import com.mysql.cj.jdbc.MysqlDataSource;
+
 import java.sql.*;
 
 public class TestDeConnection {
     public static void main(String... args){
         Connection conn = null;
         try {
-            //MySQL driver MySQL Connector
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris","root","root");
+            //MySQL using dataSource
+            MysqlDataSource dataSource = new MysqlDataSource();
 
-            conn.setAutoCommit(false);
+            //dataSource.setUrl("jdbc:mysql://localhost:3306/tennis?useSSL=false&useLegacyDatetimeCode=false&serverTimezone=Europe/Paris");
+            dataSource.setServerName("localhost");
+            dataSource.setPort(3306);
+            dataSource.setDatabaseName("tennis");
+            dataSource.setUseSSL(false);
+            dataSource.setUser("root");
+            dataSource.setPassword("root");
 
-            PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO JOUEUR (NOM,PRENOM,SEXE) VALUES (?,?,?)");
+            conn = dataSource.getConnection();
 
-            String nom = "Capriati";
-            String prenom = "Jennifer";
-            String sexe = "F";
-            preparedStatement.setString(1, nom);
-            preparedStatement.setString(2, prenom);
-            preparedStatement.setString(3, sexe);
+//            conn.setAutoCommit(false);
+//
+//            PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO JOUEUR (NOM,PRENOM,SEXE) VALUES (?,?,?)");
+//
+//            String nom = "Capriati";
+//            String prenom = "Jennifer";
+//            String sexe = "F";
+//            preparedStatement.setString(1, nom);
+//            preparedStatement.setString(2, prenom);
+//            preparedStatement.setString(3, sexe);
+//
+//            preparedStatement.executeUpdate();
+//
+//            nom = "Johannson";
+//            prenom = "Thomas";
+//            sexe = "M";
+//            preparedStatement.setString(1, nom);
+//            preparedStatement.setString(2, prenom);
+//            preparedStatement.setString(3, sexe);
+//
+//            preparedStatement.executeUpdate();
+//
+//            conn.commit();
 
-            preparedStatement.executeUpdate();
-
-            nom = "Johannson";
-            prenom = "Thomas";
-            sexe = "M";
-            preparedStatement.setString(1, nom);
-            preparedStatement.setString(2, prenom);
-            preparedStatement.setString(3, sexe);
-
-            preparedStatement.executeUpdate();
-
-            conn.commit();
-
-            System.out.println("success");
+            System.out.println("success "+conn);
         } catch (SQLException e) {
             e.printStackTrace();
         }
